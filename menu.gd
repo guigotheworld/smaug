eed48bf3d3e8c285267733e74bf0479258f671ac
Guilherme Brito <guilherme.brtsv@outlook.com>
2024-10-12 21:51:29 -0300

    Initial commit

---
+++ menu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on_jogar_pressed() -> void:
+	get_tree().change_scene_to_file("res://cenas.tscn")
+
+
+func _on_opções_pressed() -> void:
+
+
+
+func _on_sair_pressed() -> void:
+	get_tree().quit()
